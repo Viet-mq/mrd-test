f546c10dbe0adee06a55e79c358f115b4d4d3adf
vietmq <vietmq@viettel.com.vn>
2024-02-27 20:50:11 +0700

    first commit

---
+++ src/test/java/com/example/mrdtest/MrdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mrdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
